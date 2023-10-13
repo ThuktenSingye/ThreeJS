be055739e0df5b80c76b734b4460abc3be710cf9
ThuktenSingye <thuktensingye2163@gmail.com>
2023-10-13 20:24:00 +0600

    updated rotation based on previous frame speed #delta

--- Tutorial/FirstApp/src/World/components/cube.js
+++ Tutorial/FirstApp/src/World/components/cube.js
@@ -1,4 +1,5 @@
 import { BoxGeometry , Mesh, MeshStandardMaterial , MeshBasicMaterial, Vector3, MathUtils} from 'three';
+import { randFloat } from 'three/src/math/MathUtils';
 
 
 function createCube(){
@@ -11,7 +12,16 @@ function createCube(){
 
     // create mesh containing the geometry and material
     const cube = new Mesh(geometry , material);
-   
+    
+    // the cube will rotate 30 second per second and will play at same speed no matter where itrun
+    const radianPerSecond = MathUtils.degToRad(30);
+
+    cube.tick = (delta) =>{
+        // increase the cube rotation each frame
+        cube.rotation.z += radianPerSecond * 0.01;
+        cube.rotation.x += radianPerSecond * 0.01;
+        cube.rotation.y += radianPerSecond * 0.01;
+    };
 
 
     return cube;
