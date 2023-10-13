b64e5a6c0455f760166a059fb21f57394c3375c2
ThuktenSingye <thuktensingye2163@gmail.com>
2023-10-13 20:23:10 +0600

    Remove resizer event

--- Tutorial/FirstApp/src/World/systems/Resizer.js
+++ Tutorial/FirstApp/src/World/systems/Resizer.js
@@ -1,17 +1,27 @@
+
+const setSize = (container , camera, renderer) =>{
+    // set the camera aspect ratio
+    camera.aspect = container.clientWidth / container.clientHeight;
+
+    // update the camera's frustum
+    camera.updateProjectionMatrix();
+
+    // update the size of the renderer AND the canvas
+    renderer.setSize(container.clientWidth, container.clientHeight);
+
+    // set the pixel ratio(for mobile devices)
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+}   
 class Resizer{
     constructor(container , camera, renderer){
-        // set the camera aspect ratio
-        camera.aspect = container.clientWidth / container.clientHeight;
+        // set the initial size on load
+        setSize(container , camera, renderer);
 
-        // update the camera's frustum
-        camera.updateProjectionMatrix();
-
-        // update the size of the renderer AND the canvas
-        renderer.setSize(container.clientWidth, container.clientHeight);
-
-        // set the pixel ratio(for mobile devices)
-        renderer.setPixelRatio(window.devicePixelRatio);
-
+        // window.addEventListener("resize", ()=>{
+        //     // set the size again if the resize occures
+        //     setSize(container, camera, renderer);
+        // })
     }
 }
 
