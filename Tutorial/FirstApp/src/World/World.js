bf6829b3a8899a8125988b21cfa548b906f29cee
ThuktenSingye <thuktensingye2163@gmail.com>
2023-10-13 20:22:33 +0600

    set up animation function #start#stop

--- Tutorial/FirstApp/src/World/World.js
+++ Tutorial/FirstApp/src/World/World.js
@@ -4,42 +4,42 @@ import { createLight } from "./components/light";
 import { createScene } from "./components/scene";
 import { Resizer } from "./systems/Resizer";
 import { createRenderer } from "./systems/renderer";
+import { Loop } from "./systems/loop";
 
 // below variables are module-scoped and cannot be accessed outside the module
 let camera;
 let renderer;
 let scene;
 let cube;
+let loop;
 class World {
     constructor(container) {
         camera = createCamera();
         scene = createScene();
         renderer = createRenderer();
+        loop = new Loop(camera, scene , renderer);
         container.append(renderer.domElement);
 
-        // const origin = new Vector3();
-        // console.log(origin.x, origin.y, origin.z);
-
-        cube = createCube();
      
+        cube = createCube();
         const light = createLight();
 
+        loop.updatables.push(cube);
         scene.add(cube, light);
-        console.log(scene.children) // give children 
+    
         const resizer = new Resizer(container, camera, renderer);
-        // Bind the renderScene function to the class instance
-        this.renderScene = this.renderScene.bind(this);
         
     }
 
     renderScene() {
-        // draw a single frameset
-        requestAnimationFrame(this.renderScene);
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-
+        //  draw a single frameset
         renderer.render(scene, camera);
+    }
+    start(){
+        loop.start();
+    }
+    stop(){
+        loop.stop();
     }
 }
 
